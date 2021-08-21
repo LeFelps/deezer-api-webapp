35d23ebc5eef782dc65d025c586bca32ec5a72ac
LeFelps <LeFelps@hotmail.com>
2021-08-21 06:24:22 -0300

    first version of app

--- src/App.js
+++ src/App.js
@@ -1,57 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import SongList from './app/components/song-list/SongList';
+import Favorites from './app/components/favorites/Favorites';
+import Navbar from './app/components/navbar/Navbar.js'
 import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header"> 
+        </header>
+          <Navbar />
+          <div className="content">
+              <Route exact path='/' component={SongList}></Route>
+              <Route path='/favorites' component={Favorites}></Route>
+          </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
