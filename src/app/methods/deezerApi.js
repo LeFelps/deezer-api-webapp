bc018954c05ee5f31209bd8f5202a29a199579c7
LeFelps <LeFelps@hotmail.com>
2021-08-24 12:48:46 -0300

    cleaning code

--- src/app/methods/deezerApi.js
+++ src/app/methods/deezerApi.js
@@ -1,14 +1,7 @@
 import axios from "axios";
 
-// var config = {
-//                 method: 'get',
-//                 url: '/chart/tracks?index=0&limit=100',
-//             };
-
-
 export function getSongData(config) {
     return new Promise((resolve) =>
-    //   setTimeout(() => resolve({ data: amount }), 500)
         axios(config)
             .then(res => {
                 resolve = res.data.tracks.data
@@ -19,33 +12,4 @@ export function getSongData(config) {
             
     );
   }
-
-
-// export const getSongs = () =>{
-
-//     return (dispatch, getState) => {
-
-        
-//         var config = {
-//             method: 'get',
-//             url: '/chart/tracks?index=0&limit=100',
-//         };
-    
-//         var response
-
-//         axios(config)
-//             .then(res => {
-//                 response = res.data.tracks.data
-//             })
-//             .catch(error => {
-//                 console.log(error);
-//             });
-
-//             console.log(response);
-
-
-//       dispatch({ type: 'GET_SONGS', songs: response })
-//     }
-
-// } 
 
