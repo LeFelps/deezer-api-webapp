1e157b39a235dc40372e884e7016ecdd21412532
LeFelps <LeFelps@hotmail.com>
2021-08-24 22:36:59 -0300

    improvement on audio player behavior

--- src/app/components/player/Player.js
+++ src/app/components/player/Player.js
@@ -6,6 +6,13 @@ import { secondsToMinutes } from '../../methods/songMethods';
 var audio = new Audio();
 var playing = false
 
+const autoPause = (track) =>{
+    if (playing === true){
+        audio.pause()
+        playing = false
+    } 
+}
+
 const playPause = (track) =>{
 
         if (playing === true){
@@ -21,8 +28,8 @@ const playPause = (track) =>{
 }
 
 const Player = ({ activeSong, dispatch }) => (
-    
     <div className="footer flex">
+        {autoPause(activeSong)}
         <div className="play-button" onClick={() => playPause(activeSong)} >
             <svg className="play-button-icon" focusable="false" height="" role="img" width="22" viewBox="0 0 12 12" aria-hidden="true"><path clipRule="evenodd" fillRule="evenodd"  d="M2.5.5v11l9-5.5z"></path></svg>
         </div>
